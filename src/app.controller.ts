e323202867992630c73b884824b66240f661b832
Dudu Neto <dudumatosneto@gmail.com>
2022-05-24 14:48:53 -0300

    create product event emitter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateProductInputDTO } from './dtos/product/create-product.dto';
 
-@Controller()
+@Controller('v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private clientAdminBackend: ClientProxy;
 
-  @Get()
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: { urls: ['amqp://localhost:5672'], queue: 'admin-backend' },
+    });
+  }
+
+  @Get('version')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Api Version 1.0.0';
+  }
+
+  @Post('product')
+  async createProduct(@Body() createProductDto: CreateProductInputDTO) {
+    return await this.clientAdminBackend.emit(
+      'create-product',
+      createProductDto,
+    );
   }
 }
